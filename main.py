8c23959f9f9377a30f0709614b27ee9ef1e228d9
jasmineteters <jasmineteters2020@gmail.com>
2022-08-23 21:57:48 -0700

    Added error handling and changed the file format to JSON instead of txt

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
-import csv
+import json
+
 Y_PADDING = 5
 BUTTON_HEIGHT = 1
 YOUR_EMAIL_ADDRESS = 'myEmail@email.com'
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -29,6 +33,8 @@ def generate_password():
         password_entry.delete(0, END)
         password_entry.insert(0, password)
         pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -36,23 +42,34 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    concat_entry = f"{website} | {email} | {password}\n"
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        confirm_save = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {email}\n"
-                                                                 f"Password: {password}\nIs it okay to save?")
-
-        if confirm_save == True:
-            with open("data.txt", "a") as data_file:
-                data_file.write(concat_entry)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             # Clearing the entries
             website_entry.delete(0, END)
             password_entry.delete(0, END)
             # Resetting the focus to the website
             website_entry.focus()
             messagebox.showinfo(title="Success", message="Your password was saved")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -61,13 +78,13 @@ def save():
 
 window = Tk()
 window.title("MyPass Manager")
-window.config(padx=100, pady=100, bg= 'white')
+window.config(padx=100, pady=100, bg='white')
 
 # TODO Canvas
-canvas = Canvas(width=200, height=200, bg="white",highlightthickness=0)
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0,column=1)
+canvas.grid(row=0, column=1)
 
 # ---------------------------- LABELS ------------------------------- #
 
@@ -80,7 +97,6 @@ website_label.grid(column=0, row=1)
 email_label = Label(bg="white", pady=Y_PADDING)
 email_label.config(text="Email/Username: ", font=("Times New Roman", 14))
 email_label.grid(column=0, row=2)
-
 
 # TODO Password Label
 website_label = Label(bg="white", pady=Y_PADDING)
@@ -114,14 +130,5 @@ generate_password.grid(row=3, column=2)
 generate_password = Button(height=BUTTON_HEIGHT, width=45, pady=Y_PADDING, command=save)
 generate_password.config(text="Add")
 generate_password.grid(row=4, column=1, columnspan=2)
-
-
-
-
-
-
-
-
-
 
 window.mainloop()
